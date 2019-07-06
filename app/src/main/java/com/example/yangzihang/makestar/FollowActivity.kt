21084ea15b5b625d003dc43e8f08cfd503ab91be
Yookiely <zihang1998115@126.com>
2019-07-06 10:46:23 +0800

    写了更多的View

---
+++ app/src/main/java/com/example/yangzihang/makestar/FollowActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yangzihang.makestar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FollowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_follow)
+    }
+}

329d947bf66b853a3773aff382041f280addfac1
Yookiely <zihang1998115@126.com>
2019-07-11 11:13:52 +0800

    引入了全新的依赖，完善了"我的"页面

--- app/src/main/java/com/example/yangzihang/makestar/FollowActivity.kt
+++ app/src/main/java/com/example/yangzihang/makestar/FollowActivity.kt
@@ -2,7 +2,11 @@ package com.example.yangzihang.makestar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Window
+import cn.edu.twt.retrox.recyclerviewdsl.withItems
+import com.example.yangzihang.makestar.View.setUserText
 
 class FollowActivity : AppCompatActivity() {
 
@@ -10,5 +14,15 @@ class FollowActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_follow)
+        val recyclerView  = findViewById<RecyclerView>(R.id.user_rec)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.withItems{
+            setUserText("历史排名")
+            setUserText("我的消息")
+            setUserText("我的收藏")
+            setUserText("我的作品")
+            setUserText("我的订单")
+            setUserText("商务合作")
+        }
     }
 }
---
+++ app/src/main/java/com/example/yangzihang/makestar/View/UserItem.kt
@@ -0,0 +1,43 @@
+package com.example.yangzihang.makestar.View
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import cn.edu.twt.retrox.recyclerviewdsl.Item
+import cn.edu.twt.retrox.recyclerviewdsl.ItemController
+import com.example.yangzihang.makestar.R
+import org.jetbrains.anko.layoutInflater
+
+class UserItem(val titles : String) : Item{
+
+
+    private companion object Controller : ItemController{
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: Item) {
+            holder as UserItemViewHolder
+            item  as  UserItem
+            holder.title.text = item.titles
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
+            val inflater = parent.context.layoutInflater
+            val view = inflater.inflate(R.layout.user_item_my, parent, false)
+            return UserItemViewHolder(view)
+
+        }
+
+        private class UserItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+            val title = itemView.findViewById<TextView>(R.id.user_text)
+        }
+
+    }
+
+
+    override val controller: ItemController
+    get() = UserItem
+
+
+}
+
+
+fun MutableList<Item>.setUserText(titles: String) = add(UserItem(titles))

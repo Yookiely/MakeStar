d876086b2eb1249bb80c1a37c5a9763da0fc6498
WingedVampires <lsc990312@tju.edu.cn>
2019-07-18 22:37:25 +0800

    新增了关注模块，并写了部分布局；写了主页模块中的布局

---
+++ attention/src/main/java/com/wingedvampires/attention/AttentionActivity.kt
@@ -0,0 +1,12 @@
+package com.wingedvampires.attention
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AttentionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_attention)
+    }
+}

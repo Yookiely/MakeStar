7fd584d8121b899e212cc15f148ff8b88fc1ff93
Yookiely <zihang1998115@126.com>
2019-07-18 11:46:42 +0800

    开始写发现模块，写了部分布局

---
+++ discover/src/main/java/com/example/discover/DiscoverActivity.kt
@@ -0,0 +1,12 @@
+package com.example.discover
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DiscoverActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discover)
+    }
+}

5cd251c95289bb848800ae081f871cc950101317
Yookiely <zihang1998115@126.com>
2019-08-02 16:04:15 +0800

    搭建基础缓存架构

---
+++ auth/src/main/java/com/example/auth/api/AuthProvider.kt
@@ -0,0 +1,18 @@
+package com.example.auth.api
+
+import com.example.common.AuthService
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+
+//fun login(username: String, password: String, callback: suspend (RefreshState<Unit>) -> Unit = {}) {
+//    launch(UI) {
+//        AuthService.getToken(username, password).awaitAndHandle {
+//            callback(RefreshState.Failure(it))
+//        }?.data?.let {
+//            CommonPreferences.token = it.token
+//            CommonPreferences.password = password
+//            CommonPreferences.isLogin = true
+//            callback(RefreshState.Success(Unit))
+//        }
+//    }
+//}
---
+++ commons/src/main/java/com/example/common/experimental/network/UserAgent.kt
@@ -0,0 +1,1 @@
+package com.example.common.experimental.network

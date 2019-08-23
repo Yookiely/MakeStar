5ca8fed77a1ddc044cff2b60385c4422587f3d43
Yookiely <zihang1998115@126.com>
2019-08-23 16:51:47 +0800

    新增粉丝圈

---
+++ app/src/main/java/com/example/yangzihang/makestar/FansActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yangzihang.makestar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FansActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fans)
+    }
+}

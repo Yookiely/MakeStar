d9e786c738c45d64d7e00408064adf62e568f5cf
lsc99 <lsc990312@icloud.com>
2020-03-30 16:33:52 +0800

    修改了退出的xiaobug； 改了布局

--- app/src/main/java/com/example/yangzihang/makestar/SettingsActivity.kt
+++ app/src/main/java/com/example/yangzihang/makestar/SettingsActivity.kt
@@ -2,9 +2,9 @@ package com.example.yangzihang.makestar
 
 import android.app.AlertDialog
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.Window
@@ -16,6 +16,7 @@ import com.example.yangzihang.makestar.View.setNormalItem
 import com.yookie.auth.LoginActivity
 import com.yookie.common.experimental.preference.CommonPreferences
 
+
 class SettingsActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
 
@@ -26,11 +27,12 @@ class SettingsActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.settings_rec)
         recyclerView.layoutManager = LinearLayoutManager(this)
         val localLayoutParams = window.attributes
-        localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or localLayoutParams.flags)
+        localLayoutParams.flags =
+            (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or localLayoutParams.flags)
         recyclerView.withItems {
             setGreyText("账号")
-            setNormalItem("账号设置"){
-                startActivity(Intent(this@SettingsActivity,EditActivity::class.java))
+            setNormalItem("账号设置") {
+                startActivity(Intent(this@SettingsActivity, EditActivity::class.java))
 
             }
 //            setNormalItem("邀请好友"){
@@ -40,38 +42,40 @@ class SettingsActivity : AppCompatActivity() {
 //
 //            }
             setGreyText("关于")
-            setNormalItem("隐私协议"){
-                val intent = Intent(this@SettingsActivity,AgreementActivity::class.java)
-                intent.putExtra("FLAG","rule")
+            setNormalItem("隐私协议") {
+                val intent = Intent(this@SettingsActivity, AgreementActivity::class.java)
+                intent.putExtra("FLAG", "rule")
                 startActivity(intent)
             }
-            setNormalItem("用户协议"){
-                val intent = Intent(this@SettingsActivity,AgreementActivity::class.java)
-                intent.putExtra("FLAG","agreement")
+            setNormalItem("用户协议") {
+                val intent = Intent(this@SettingsActivity, AgreementActivity::class.java)
+                intent.putExtra("FLAG", "agreement")
                 startActivity(intent)
             }
 //            setNormalItem("意见反馈"){
 //
 //            }
-            setNormalItem("关于V-star"){
-                val intent = Intent(this@SettingsActivity,AgreementActivity::class.java)
-                intent.putExtra("FLAG","about")
+            setNormalItem("关于V-star") {
+                val intent = Intent(this@SettingsActivity, AgreementActivity::class.java)
+                intent.putExtra("FLAG", "about")
                 startActivity(intent)
             }
             setGreyText("  ")
-            setNormalItem("退出登录"){
+            setNormalItem("退出登录") {
                 AlertDialog.Builder(this@SettingsActivity)
                     .setMessage("真的要登出吗？")
                     .setPositiveButton("真的") { _, _ ->
-                        PreferenceManager.getDefaultSharedPreferences(this@SettingsActivity).edit().clear().apply()
+                        PreferenceManager.getDefaultSharedPreferences(this@SettingsActivity).edit()
+                            .clear().apply()
                         CommonPreferences.clear()
                         val intent = Intent(this@SettingsActivity, LoginActivity::class.java)
-
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                         this@SettingsActivity.startActivity(intent)
-                        this@SettingsActivity.finish()
                     }
                     .setNegativeButton("算了") { _, _ ->
-                        Toast.makeText(this@SettingsActivity, "真爱啊 TAT...", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@SettingsActivity, "真爱啊 TAT...", Toast.LENGTH_SHORT)
+                            .show()
                     }.create().show()
             }
         }
